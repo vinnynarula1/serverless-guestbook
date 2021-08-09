2cd25053e77a871f1a94d23be279a261fb876044
vinnynarula1 <84547910+vinnynarula1@users.noreply.github.com>
2021-08-09 00:11:55 -0700

    Updated API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a1d0baed.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
